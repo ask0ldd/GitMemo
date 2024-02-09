63357b3cd00edc789637114ed3bef6b450e0109a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-10 00:52:46 +0100

    commandline compo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import CommandLine from './components/CommandLine'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div style={{width:'800px'}}>
+      <h2>Git Branch</h2>
+      <CommandLine>git branch newImage</CommandLine>
+      <p>Create a newImage branch at the HEAD position.</p>
+      <p>The HEAD doesn't get attached to the new branch.</p>
+      <br/>
+      <p style={{display:'flex', justifyContent:'center', width:'100%'}}>C1 - - - C2 [main*] ={'>'} C1 - - - C2 [main* newImage] </p>
+      <br/>
+      <CommandLine>git branch newImage</CommandLine>
+      <CommandLine>git commit</CommandLine>
+      <p>The HEAD is attached to MAIN.</p>
+      <p>Create a newImage branch at the HEAD position.</p>
+      <br/>
+      <p style={{display:'flex', justifyContent:'center', width:'100%'}}>C1 - - - C2 [main*] ={'>'} C1 - - - C2 [newImage] - - - C3 [main*] </p>
+    </div>
   )
 }
 
---
+++ src/components/CommandLine.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function CommandLine(props : React.PropsWithChildren) {
+  return (
+    <p style={style}>{'>'}  {props.children}</p>
+  )
+}
+
+const style = {
+    height:'3rem',
+    backgroundColor: '#111111',
+    color: '#EEEEEE',
+    display: 'flex',
+    alignItems: 'center',
+    padding : '0 1rem',
+    marginBottom: '1rem',
+    marginTop: '1rem',
+}
