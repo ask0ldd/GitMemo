b5ca9c6a9f5a987495260ab4425bf8044248b084
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-02-11 12:56:28 +0100

    css mod to improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -49,74 +49,92 @@ function App() {
       <hr/>
       <br/>
       <h2>Git Merge</h2>
-      <CommandLine>git merge bugFix</CommandLine>
-      <p>The HEAD is attached to MAIN.</p>
-      <p>MAIN moves to a new commit.</p>
-      <p>This new commit contains C2 & C3.</p>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git merge bugFix</CommandLine>
+          <p>The HEAD is attached to MAIN.</p>
+          <p>MAIN moves to a new commit.</p>
+          <p>This new commit contains C2 & C3.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
-      <br/>
-      <CommandLine>git checkout bugFix</CommandLine>
-      <CommandLine>git merge main</CommandLine>
-      <p>The HEAD moves & attaches itself to bugFix.</p>
-      <p>bugFix is an ancestor of C4 so when merging, bugFix moves to C4.</p>
-      <p>bugFix now contains C1, C2, C3 & C4.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{padding:'1rem 0'}}>
+          <CommandLine>git checkout bugFix</CommandLine>
+          <CommandLine>git merge main</CommandLine>
+          <p>The HEAD moves & attaches itself to bugFix.</p>
+          <p>bugFix is an ancestor of C4 so when merging, bugFix moves to C4.</p>
+          <p>bugFix now contains C1, C2, C3 & C4.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
       <hr/>
       <br/>
       <h2>Git Rebase</h2>
-      <CommandLine>git rebase main</CommandLine>
-      <p>The HEAD is attached to bugFix.</p>
-      <p>A copy of the bugFix current commit is placed under the current MAIN commit.</p>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git rebase main</CommandLine>
+          <p>The HEAD is attached to bugFix.</p>
+          <p>A copy of the bugFix current commit is placed under the current MAIN commit.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
-      <br/>
-      <CommandLine>git rebase bugFix</CommandLine>
-      <p>The HEAD is attached to MAIN.</p>
-      <p>MAIN moves to include C3'.</p>
-      <p>Equivalent to git merge bugFix.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{padding:'1rem 0'}}>
+          <CommandLine>git rebase bugFix</CommandLine>
+          <p>The HEAD is attached to MAIN.</p>
+          <p>MAIN moves to include C3'.</p>
+          <p>Equivalent to git merge bugFix.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
       <hr/>
       <br/>
       <h2>Moving the HEAD</h2>
-      <CommandLine>git checkout C1</CommandLine>
-      <p>Move the HEAD to C1.</p>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git checkout C1</CommandLine>
+          <p>Move the HEAD to C1.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{padding:'1rem 0'}}>
+          <CommandLine>git checkout main~2</CommandLine>
+          <p>Move the HEAD two commits behind MAIN.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
-      <CommandLine>git checkout main~2</CommandLine>
-      <p>Move the HEAD two commits behind MAIN.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
-      <br/>
-      <CommandLine>git branch -f main HEAD~2</CommandLine>
-      <p>the HEAD is on bugFix.</p>
-      <p>MAIN & bugFix are on C3.</p>
-      <p>Move MAIN to C1.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git branch -f main HEAD~2</CommandLine>
+          <p>the HEAD is on bugFix.</p>
+          <p>MAIN & bugFix are on C3.</p>
+          <p>Move MAIN to C1.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
       <hr/>
       <br/>
       <h2>Git Reset</h2>
-      <CommandLine>git reset HEAD~1</CommandLine>
-      <p>Move one commit back.</p>
-      <p>Soft delete the commit where the HEAD was.</p>
-      <p>A new anonymous branch will be created to put the next new commit.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git reset HEAD~1</CommandLine>
+          <p>Move one commit back.</p>
+          <p>Soft delete the commit where the HEAD was.</p>
+          <p>A new anonymous branch will be created to put the next new commit.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
       <hr/>
       <br/>
       <h2>Git Revert</h2>
-      <CommandLine>git revert HEAD</CommandLine>
-      <p>Create a new commit cloning the commit right before the head.</p>
-      <br/>
-      <GitTree>aaa{"=>"}bbb</GitTree>
+      <div style={{backgroundColor:'#c7ddf0', padding:'1rem 0'}}>
+          <CommandLine>git revert HEAD</CommandLine>
+          <p>Create a new commit cloning the commit right before the head.</p>
+          <br/>
+          <GitTree>aaa{"=>"}bbb</GitTree>
+      </div>
       <br/>
     </div>
   )
